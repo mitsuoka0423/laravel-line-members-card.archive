fa04bb63d970e0a87d17d601db064673264fcbf9
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2022-06-03 00:28:36 +0900

    add page component

--- frontend/src/components/card/index.tsx
+++ frontend/src/components/card/index.tsx
@@ -7,16 +7,14 @@ interface CardPropsInterface {
 
 export const Card = (props: CardPropsInterface) => {
   return (
-    <Box margin="40px">
-      <AspectRatio ratio={1.618 / 1}>
-        <Box boxShadow="base" borderRadius="20px">
-          {props.userId ? (
-            <Barcode id={props.userId}></Barcode>
-          ) : (
-            <Skeleton width="50%" height="80px"></Skeleton>
-          )}
-        </Box>
-      </AspectRatio>
-    </Box>
+    <AspectRatio ratio={1.618 / 1}>
+      <Box boxShadow="base" borderRadius="20px">
+        {props.userId ? (
+          <Barcode id={props.userId}></Barcode>
+        ) : (
+          <Skeleton width="50%" height="100px"></Skeleton>
+        )}
+      </Box>
+    </AspectRatio>
   );
 };
