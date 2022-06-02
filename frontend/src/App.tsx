3a107bb1eb12e2c475803591336a07c64001c608
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2022-06-02 21:13:28 +0900

    add barcode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,45 @@
-import { useEffect, useState } from "react";
-import liff from "@line/liff";
-import "./App.css";
+import { useEffect, useState } from 'react';
+import liff from '@line/liff';
+import { useBarcode } from 'react-barcodes';
+import './App.css';
 
 function App() {
-  const [message, setMessage] = useState("");
-  const [error, setError] = useState("");
+    const [message, setMessage] = useState('');
+    const [error, setError] = useState('');
+    const { inputRef } = useBarcode({ value: 'react' });
 
-  useEffect(() => {
-    liff
-      .init({
-        liffId: import.meta.env.VITE_LIFF_ID
-      })
-      .then(() => {
-        setMessage("LIFF init succeeded.");
-      })
-      .catch((e: Error) => {
-        setMessage("LIFF init failed.");
-        setError(`${e}`);
-      });
-  });
+    useEffect(() => {
+        liff.init({
+            liffId: import.meta.env.VITE_LIFF_ID,
+        })
+            .then(() => {
+                setMessage('LIFF init succeeded.');
+            })
+            .catch((e: Error) => {
+                setMessage('LIFF init failed.');
+                setError(`${e}`);
+            });
+    });
 
-  return (
-    <div className="App">
-      <h1>create-liff-app</h1>
-      {message && <p>{message}</p>}
-      {error && (
-        <p>
-          <code>{error}</code>
-        </p>
-      )}
-      <a
-        href="https://developers.line.biz/ja/docs/liff/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        LIFF Documentation
-      </a>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>create-liff-app</h1>
+            {message && <p>{message}</p>}
+            {error && (
+                <p>
+                    <code>{error}</code>
+                </p>
+            )}
+            <a
+                href="https://developers.line.biz/ja/docs/liff/"
+                target="_blank"
+                rel="noreferrer"
+            >
+                LIFF Documentation
+            </a>
+            <svg ref={inputRef} />
+        </div>
+    );
 }
 
 export default App;
